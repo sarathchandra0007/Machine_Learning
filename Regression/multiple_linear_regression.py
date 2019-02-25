ee66d88e8cad2fc2709f2dfc0aec5d7eee65b693
Sarath <sarathchandra0007@gmail.com>
2019-02-25 23:00:24 +0530

    Implemented multiple lenear regression algorithm and eliminated the fields which pvalue is greater than 0.05

--- Regression/multiple_linear_regression.py
+++ Regression/multiple_linear_regression.py
@@ -26,3 +26,38 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 
 # Fit multiple regression model to training set
 
+from sklearn.linear_model import LinearRegression
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predecting test results
+
+y_pred = regressor.predict(X_test)
+
+# Backward elimination
+
+import statsmodels.formula.api as sm
+# y = b0 + x1b1+x2b2...
+# for b0 we need to add constants in begening of table
+
+X = np.append(arr = np.ones((50,1)).astype(int), values = X, axis=1)
+X_opt = X[:, [0,1,2,3,4,5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0,1,3,4,5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0,3,4,5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0,3,5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0,3]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressor_OLS.summary()
