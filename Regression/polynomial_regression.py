5c5a92bb1da3b7fc9d38155e273ab17d6e67d45d
Sarath <sarathchandra0007@gmail.com>
2019-03-01 09:05:15 +0530

    Polynomial regression

--- Regression/polynomial_regression.py
+++ Regression/polynomial_regression.py
@@ -6,10 +6,40 @@ import pandas as pd
 
 dataset = pd.read_csv('Position_Salaries.csv')
 
-X = dataset.iloc[:, :-1].values
+X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+# For correct predections we analyze both regressions
 
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X,y)
 
-from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+pol_reg = PolynomialFeatures(degree=4)
+X_poly = pol_reg.fit_transform(X, y)
+lin_reg2 = LinearRegression()
+lin_reg2.fit(X_poly, y)
+
+# Graphicla representation
+# linear regression
+
+plt.scatter(X,y, color='blue')
+plt.plot(X, lin_reg.predict(X), color='red')
+plt.title("linear regression")
+plt.xlabel("level")
+plt.ylabel("salaries")
+plt.show()
+
+# ploynomial regression
+
+plt.scatter(X,y, color='blue')
+plt.plot(X, lin_reg2.predict(pol_reg.fit_transform(X)), color='red')
+plt.title("poly regression")
+plt.xlabel("level")
+plt.ylabel("salaries")
+plt.show()
+
+# custom predections
+
+lin_reg2.predict(pol_reg.fit_transform(12))
